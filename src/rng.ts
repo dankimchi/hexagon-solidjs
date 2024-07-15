0d538ad2954e697d1393151563c22ec5501026a2
Daniel Kim <daniel.888.kim@gmail.com>
2024-07-15 18:15:15 -0400

    set random seed

--- src/rng.ts
+++ src/rng.ts
@@ -1,3 +1,6 @@
+import seedrandom from 'seedrandom';
+
+seedrandom('eugeneAndRegina', { global: true });
 
 export default function rng(min: number, max: number) {
   return Math.round(Math.random() * (max - min)) + min;
